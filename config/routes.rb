3e0f55083fe7a6e5bdc1d52fbfe9512f3524225d
Mohammad Khan <mkhan138@mail.depaul.edu>
2017-11-14 21:54:30 +0000

    checkout

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  resources :orders
+  root 'store#index', as: 'store_index'
+  resources :line_items
+  resources :carts
+  get 'store/index'
+  
+  resources :users
+  resources :products
+  #root 'products#index'
+  root 'store#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root 'products#index'
+end
